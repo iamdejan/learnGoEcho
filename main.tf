44bfc2863b6353d0cde0ba4da662e675a1d1bdb8
Giovanni Dejan <giovanni.dejan@traveloka.com>
2022-04-08 22:26:50 +0700

    Refactor pipeline

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 # Semacem import untuk nunjukin kita ngambil resource dari mana
 # dalam case ini kita import gogole, karna kita butuh resource google
 provider "google" {
-  project = "fabled-citadel-342513"
-  region = "asia-southeast2"
-  zone = "asia-southeast2-a"
+  project = var.project
+  region  = "asia-southeast2"
+  zone    = "asia-southeast2-a"
 }
 
 # Deploy image to Cloud Run
 # ngebuat resource di gcp dengan template/tipe resource google_cloud_run_service
 resource "google_cloud_run_service" "my_web_app" {
   # field nama resourcenya
-  name = "my-web-app"
+  name     = "my-web-app"
   location = "asia-southeast2"
   template {
     spec {
       containers {
-        image = "asia.gcr.io/fabled-citadel-342513/learn-go-echo"
+        image = "asia.gcr.io/${var.project}/learn-go-echo"
         ports {
           container_port = 1111
         }
@@ -23,7 +23,7 @@ resource "google_cloud_run_service" "my_web_app" {
     }
   }
   traffic {
-    percent = 100
+    percent         = 100
     latest_revision = true
   }
 }
@@ -36,16 +36,16 @@ data "google_iam_policy" "no_auth" {
     members = [
       "allUsers",
     ]
-    role    = "roles/run.invoker"
+    role = "roles/run.invoker"
   }
 }
 
 # Enable public access on Cloud Run service
 # buat kasih cloud run gw, siapa aja yang boleh cloud run gw, dengan cara ngetaro policy_data yang dah didapet
 resource "google_cloud_run_service_iam_policy" "no_auth" {
-  location = google_cloud_run_service.my_web_app.location
-  project = google_cloud_run_service.my_web_app.project
-  service = google_cloud_run_service.my_web_app.name
+  location    = google_cloud_run_service.my_web_app.location
+  project     = google_cloud_run_service.my_web_app.project
+  service     = google_cloud_run_service.my_web_app.name
   policy_data = data.google_iam_policy.no_auth.policy_data
 }
 
